e3e5abb558144e6f70b933f40a7af35d3e642d94
krzychna33 <krzychna33@gmail.com>
2017-10-08 14:51:30 +0200

    Add get /todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,17 @@ app.post('/todos', (req, res) =>{
   })
 });
 
+app.get('/todos', (req, res) =>{
+  Todo.find().then((todos) =>{
+    res.send({
+      todos,
+      time: new Date()
+    })
+  }, (e) =>{
+    res.status(400).send(e);
+  })
+})
+
 
 
 app.listen(3000, ()=>{
