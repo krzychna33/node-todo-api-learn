cd0b9e5610fb087b331b93ad277e7282b663f54f
krzychna33 <krzychna33@gmail.com>
2017-11-04 23:57:06 +0100

    Add private routes middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 
@@ -99,6 +100,8 @@ app.patch('/todos/:id', (req, res) =>{
   })
 });
 
+// -------------------- USER SECTION -------
+// -------------------- POST /user -------
 
 app.post('/users', (req, res) =>{
   var body = _.pick(req.body, ['email', 'password']);
@@ -112,8 +115,33 @@ app.post('/users', (req, res) =>{
   }).catch((e) =>{
     res.status(400).send(e);
   });
+});
 
-})
+
+
+app.get('/users/me', authenticate, (req, res) =>{
+  res.send(req.user);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 app.listen(port, ()=>{
