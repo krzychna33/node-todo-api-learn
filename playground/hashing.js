479fefce2fbb866d2b642139479a174186df2539
krzychna33 <krzychna33@gmail.com>
2017-11-09 20:22:02 +0100

    Hash user password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,47 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-}
+var password = '123abc';
 
-var token = jwt.sign(data, '123abc');
-console.log(token)
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash)=>{
+//     console.log(hash);
+//   });
+// });
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+
+var hashedPassword = '$2a$10$2QHP/1GbtEcNmj1dmMicDO/Kcje0K8SC/BTdZXAB1AV7oc6xVuaW.X';
+
+bcrypt.compare(password, hashedPassword, (err, res) =>{
+  console.log(res);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var data = {
+//   id: 10
+// }
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token)
+//
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 
 
