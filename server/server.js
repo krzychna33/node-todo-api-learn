8893fceaf4dbacb52f5809ca5bbf26e5f09538e5
krzychna33 <krzychna33@gmail.com>
2017-11-12 00:22:50 +0100

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const bcrypt = require('bcryptjs');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -117,6 +118,35 @@ app.post('/users', (req, res) =>{
   });
 });
 
+
+app.post('/users/login', (req, res) =>{
+  var body = _.pick(req.body, ['email', 'password']);
+
+
+  User.findByCredentials(body.email, body.password).then((user) =>{
+    return user.generateAuthToken().then((token) =>{
+      res.header('x-auth', token).send(user);
+    })
+  }).catch((e) =>{
+    res.status(400).send(e);
+  })
+
+  // User.findOne({
+  //   email: body.email,
+  // }).then((user) =>{
+  //   bcrypt.compare(body.password, user.password, (err, accessRes) =>{
+  //     if(accessRes){
+  //       res.send(user)
+  //     } else {
+  //       res.status(403).send('Bad password');
+  //     }
+  //   })
+  // }).catch((e) =>{
+  //   res.status(404).send(e);
+  // });
+
+
+})
 
 
 app.get('/users/me', authenticate, (req, res) =>{
